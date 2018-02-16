8b0bd2ef67385a6b67a61f3129644d6f58a6c75c
Tyler Edwards <te413803@gmail.com>
2018-02-15 17:01:02 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
+
